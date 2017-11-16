151d61896b25d12db9cbae130beae2a79e6055b6
yangmingming <yangmingming@91guoxin.com>
2017-11-16 15:23:51 +0800

    修改快讯接口，去掉一些带关键词的内容

--- app/Http/Controllers/KuaixunController.php
+++ app/Http/Controllers/KuaixunController.php
@@ -9,8 +9,29 @@ class KuaixunController extends Controller
 {
     public function getkx(Request $request) {
         $kuaixun = new Kuaixun();
+        $platform = $request->input('p', 'pc');
 
         $ret = $kuaixun->getKuaixun($request->input('page'), $request->input('num'));
+
+        foreach ($ret as $key=>$val) {
+            if($this->inWords($val->body, ['jin10.com', 'fx678.com', 'wallstreetcn.com', '金十'])) {
+                unset($ret[$key]);
+            }
+            elseif ($platform == 'app') {
+                $ret[$key]->body = strip_tags($val->body);
+            }
+        }
+
         return ['success'=>1, 'value'=>$ret];
+    }
+
+    private function inWords($str, $keys) {
+        foreach($keys as $key) {
+            if (strstr($str, $key)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
