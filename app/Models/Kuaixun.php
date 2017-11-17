ad26688e81da7999cfa28c5afb57ac4c437dfafb
yangmingming <yangmingming@91guoxin.com>
2017-11-17 13:44:16 +0800

    快讯添加重要性字段

--- app/Models/Kuaixun.php
+++ app/Models/Kuaixun.php
@@ -10,11 +10,11 @@ class Kuaixun extends Model
     public function getKuaixun($page=1, $num=20) {
 
         $sql = 'select * from (';
-        $sql .= '(select id, publish_time, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "jin10" as source_site from crawl_jin10_kuaixun where real_time is null order by publish_time desc limit '.$page*$num.')';
+        $sql .= '(select id, publish_time, importance, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "jin10" as source_site from crawl_jin10_kuaixun where real_time is null order by publish_time desc limit '.$page*$num.')';
         $sql .= 'union ';
-        $sql .= '(select id, publish_time, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "fx678" as source_site from crawl_fx678_kuaixun where calendar_id is null order by publish_time desc limit '.$page*$num.') ';
+        $sql .= '(select id, publish_time, importance, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "fx678" as source_site from crawl_fx678_kuaixun where calendar_id is null order by publish_time desc limit '.$page*$num.') ';
         $sql .= 'union ';
-        $sql .= '(select id, publish_time, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "wallstreetcn" as source_site from crawl_wallstreetcn_kuaixun order by publish_time desc limit '.$page*$num.')';
+        $sql .= '(select id, publish_time, importance, dateid as source_id, body, created_time as created_at, updated_time as updated_at, "wallstreetcn" as source_site from crawl_wallstreetcn_kuaixun order by publish_time desc limit '.$page*$num.')';
         $sql .= ')all_tb order by publish_time desc limit '.($page-1)*$num .',' . $num . ';';
 
         $ret = DB::connection()->select($sql);
